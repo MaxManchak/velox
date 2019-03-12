9a9dbbb919a07951484865e324668f31789d4ada
Max Manchak <manchak.max@gmail.com>
2019-03-13 02:04:42 +0300

    added settings files

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,17 @@
-$ (document).scroll (function () {
-  var scroll = $ (window).scrollTop ();
+'use strict';
+(function () {
+	let windowHeight = $(window).height();
+	function parallax(element, rate, scroll) {
+		var temp = Math.floor((scroll / (windowHeight / 100)) * rate);
+		$(element).css({ transform: 'translateY(-' + temp + 'px)' });
+	}
 
-  parallax ($ ('.first-screen__layer--2'), 1.3, scroll);
-  parallax ($ ('.first-screen__layer--3'), 1.7, scroll);
-  parallax ($ ('.first-screen__layer--4'), 2.1, scroll);
-});
+	$(document).scroll(function () {
+		var scroll = $(window).scrollTop();
 
-var windowHeight = $ (window).height ();
-function parallax (element, rate, scroll) {
-  var temp = Math.floor (scroll / (windowHeight / 100) * rate);
-  $ (element).css ({transform: 'translateY(-' + temp + 'px)'});
-}
+		parallax($('.first-screen__layer--2'), 1.3, scroll);
+		parallax($('.first-screen__layer--3'), 1.7, scroll);
+		parallax($('.first-screen__layer--4'), 2.1, scroll);
+	});
+
+})();
